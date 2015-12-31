2af08e3517acb856fd809b4cadf67e706ce888b8
Marc-Olivier Barre <mobarre@snarchi.io>
2015-12-31 13:29:23 +0100

    Added all files to the project

--- SpaceFuckery/SpaceFuckery.cpp
+++ SpaceFuckery/SpaceFuckery.cpp
@@ -276,19 +276,3 @@ bool SpaceFuckery::quit(const CEGUI::EventArgs &e)
   mShutDown = true;
   return true;
 }
-
-extern "C" {
-int main(int argc, char *argv[])
-{
-  // Create application object
-  SpaceFuckery app;
-
-  try {
-    app.go();
-  }
-  catch(Ogre::Exception& e)  {
-    std::cerr << "An exception has occurred: " << e.getFullDescription().c_str() << std::endl;
-  }
-  return 0;
-}
-}
