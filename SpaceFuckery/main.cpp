f04ab4bf162f0188a24a42e8ffcd9a451162fa47
Marc-Olivier Barre <mobarre@snarchi.io>
2016-03-08 17:16:59 +0100

    Fix the multi-platform main.cpp

--- SpaceFuckery/main.cpp
+++ SpaceFuckery/main.cpp
@@ -1,16 +1,16 @@
+#include "Application.h"
+
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
 #endif
-
-#include "Application.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-  INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+  INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
 #else
   int main(int argc, char *argv[])
 #endif
@@ -22,51 +22,16 @@ extern "C"
     {
         SpaceFuckery::Application::getSingleton().go();
     }
-    catch( Ogre::Exception& e )
+    catch(Ogre::Exception& e)
     {
   #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+        MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
   #else
-        std::cerr << "An exception has occured: " <<
-      e.getFullDescription().c_str() << std::endl;
+        std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
   #endif
       return 0;
     }
   }
-#ifdef __cplusplus
-}
-#endif
-
-
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
-    int main(int argc, char *argv[])
-#endif
-    {
-	// Create application object
-	TutorialApplication app;
-
-	try {
-	    app.go();
-	} catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-	    MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-#else
-	    std::cerr << "An exception has occured: " <<
-		e.getFullDescription().c_str() << std::endl;
-#endif
-	}
-
-	return 0;
-    }
-
 #ifdef __cplusplus
 }
 #endif
