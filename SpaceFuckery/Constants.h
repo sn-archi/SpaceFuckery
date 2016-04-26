d5f22c966a424acf04528a9e917abe9d73baeb90
Marc-Olivier Barre <mobarre@snarchi.io>
2016-04-26 15:55:37 +0200

    Store some useful consts in a separate header

---
+++ SpaceFuckery/Constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+/*************************************************
+*                                                *
+* Project: SpaceFuckery                          *
+*                                                *
+*   Copyright 2015 - 2016 - Marc-Olivier Barre   *
+*              All rights reserved               *
+*                                                *
+**************************************************/
+
+const btScalar Pi = 3.14159265358979;
+const btScalar twoPi = 2*Pi;
+//const btScalar earthMass = 5.97237e24;
+const btScalar earthMass = 5.97237e17;
+const btScalar G = 6.6725985e-11;
+const btScalar Mu = G*earthMass;
+
+#endif // CONSTANTS_H_INCLUDED
